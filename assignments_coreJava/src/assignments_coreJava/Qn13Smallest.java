38d9c8e2a7c3422ead5c48939ff74c9a00628ca7
Jason <noronha.jason97@gmail.com>
2018-07-20 01:06:33 +0530

    qn13

---
+++ assignments_coreJava/src/assignments_coreJava/Qn13Smallest.java
@@ -0,0 +1,16 @@
+package assignments_coreJava;
+
+public class Qn13Smallest {
+
+	public static void main(String[] args) {
+		int a = 10, b = 20, c = 30;
+		if (a < b && a < c)
+			System.out.println("A smallest");
+		else if (b < a && b < c)
+			System.out.println("B is smallest");
+		else
+			System.out.println("C is smallest");
+
+	}
+
+}
